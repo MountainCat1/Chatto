0e43b3f814f94f7a329a6e1864a4461be775c131
Tomasz Banaś <72751263+MountainCat1@users.noreply.github.com>
2022-07-08 12:43:10 +0200

    Removed unused Shared project

--- Chatto/Shared/Configuration/MicroserviceSecuritySettings.cs
+++
@@ -1,9 +0,0 @@
-﻿/*namespace Shared.Configuration;
-
-public class MicroserviceSecuritySettings
-{
-    public string JwtIssuer { get; set; }
-    public double JwtExpireDays { get; set; }
-    public string JwtKey { get; set; }
-    public string ServiceName { get; set; }
-}*/
